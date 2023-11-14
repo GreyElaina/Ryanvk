aca9d3a61107e19bab5d6c9a5f75888d94c07fb0
GreyElaina <GreyElaina@outlook.com>
2023-11-14 20:37:20 +0800

    chore: remove unused NestableIterable

--- src/ryanvk/utilles.py
+++ src/ryanvk/utilles.py
@@ -8,29 +8,6 @@ from ryanvk.staff import P, R
 T = TypeVar("T")
 
 
-class NestableIterable(Iterable[T]):
-    index_stack: list
-    iterable: list[T]
-
-    def __init__(self, iterable: list[T]) -> None:
-        self.iterable = iterable
-        self.index_stack = [-1]
-
-    def __iter__(self):
-        index = self.index_stack[-1]
-        self.index_stack.append(index)
-
-        start_offset = index + 1
-        try:
-            for self.index_stack[-1], content in enumerate(
-                self.iterable[start_offset:],
-                start=start_offset,
-            ):
-                yield content
-        finally:
-            self.index_stack.pop()
-
-
 def standalone_context(func: Callable[P, R]) -> Callable[P, R]:
     @functools.wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
