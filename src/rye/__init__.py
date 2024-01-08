55b3649ca5a0bb80f853397606d8190e2a6b12cf
Elaina <GreyElaina@outlook.com>
2024-01-08 19:18:09 +0800

    chore: move namespace_generate to ops

--- src/rye/__init__.py
+++ src/rye/__init__.py
@@ -13,6 +13,7 @@ from .ops import is_implemented as is_implemented
 from .ops import isolate as isolate
 from .ops import iter_artifacts as iter_artifacts
 from .ops import layout as layout
+from .ops import namespace_generate as namespace_generate
 from .ops import provide as provide
 from .ops import shallow as shallow
 from .overloads import (
@@ -25,7 +26,6 @@ from .overloads import (
     TypeOverload as TypeOverload,
 )
 from .perform import BasePerform as BasePerform
-from .perform import namespace_generate as namespace_generate
 from .topic import PileTopic as PileTopic
 from .topic import Topic as Topic
 from .topic import merge_topics_if_possible as merge_topics_if_possible
